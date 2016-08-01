c0e78e33b15eb01a71a1f2375428b288e1b48cf7
Leonardo Rossi <leonardo.r@cern.ch>
2016-08-01 15:32:20 +0200

    examples: simple content negotiated view
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

--- examples/app.py
+++ examples/app.py
@@ -23,22 +23,86 @@
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 
-"""Minimal Flask application example for development.
+r"""Minimal Flask application example for development.
 
 Run example development server:
 
 .. code-block:: console
 
+   $ pip install -e .[all]
    $ cd examples
-   $ flask -a app.py --debug run
+   $ export FLASK_APP=app.py
+   $ flask run
+
+Load the list of records:
+
+.. code-block:: console
+
+   $ curl -v -XGET http://0.0.0.0:5000/records/?q=title:Test
+   $ curl -v -XGET http://0.0.0.0:5000/records/?q=title:Test \
+       Accept:application/xml
 """
 
 from __future__ import absolute_import, print_function
 
-from flask import Flask
+import os
+import xmlrpclib
 
-from invenio_rest import InvenioREST
+from flask import Blueprint, Flask, jsonify, make_response
+
+from invenio_rest import ContentNegotiatedMethodView, InvenioREST
+
+
+def json_v1_search(search_result):
+    """Serialize records."""
+    return make_response(jsonify(search_result))
+
+
+def xml_v1_search(search_result):
+    """Serialize records as text."""
+    return make_response(xmlrpclib.dumps((search_result,), allow_none=True))
+
+
+class RecordsListResource(ContentNegotiatedMethodView):
+    """Example Record List."""
+
+    def __init__(self, **kwargs):
+        """Init."""
+        super(RecordsListResource, self).__init__(
+            method_serializers={
+                'GET': {
+                    'application/json': json_v1_search,
+                    'application/xml': xml_v1_search,
+                },
+            },
+            default_method_media_type={
+                'GET': 'application/json',
+            },
+            default_media_type='application/json',
+            **kwargs)
+
+    def get(self, **kwargs):
+        """Implement the GET /records."""
+        return {"title": "Test"}
 
 # Create Flask application
 app = Flask(__name__)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI',
+                                           'sqlite:///app.db'),
+)
+
 InvenioREST(app)
+
+blueprint = Blueprint(
+    'mymodule',
+    __name__,
+    url_prefix='/records',
+    template_folder='templates',
+    static_folder='static',
+)
+
+records_view = RecordsListResource.as_view('records')
+blueprint.add_url_rule('/', view_func=records_view)
+
+app.register_blueprint(blueprint)
