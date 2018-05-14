4271708f0e2877e5100236be9242035b95b5ae6e
Dinos Kousidis <dinos.kousidis@cern.ch>
2018-05-14 10:58:09 +0200

    docs: point intersphinx to Flask 0.12
    Signed-off-by: Dinos Kousidis <dinos.kousidis@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -323,7 +323,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'python': ('https://docs.python.org/', None),
-    'Flask': ('http://flask.pocoo.org/docs/0.11', None),
+    'Flask': ('http://flask.pocoo.org/docs/0.12', None),
     'werkzeug': ('http://werkzeug.pocoo.org/docs/0.14', None),
 }
 
