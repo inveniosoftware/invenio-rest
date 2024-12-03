9ce258bb42b8c215fdf7b67a439457ae25658fa9
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-12-03 09:37:49 +0100

    release: v2.0.0

--- invenio_rest/__init__.py
+++ invenio_rest/__init__.py
@@ -196,6 +196,6 @@ from .csrf import csrf
 from .ext import InvenioREST
 from .views import ContentNegotiatedMethodView
 
-__version__ = "1.5.0"
+__version__ = "2.0.0"
 
 __all__ = ("__version__", "csrf", "InvenioREST", "ContentNegotiatedMethodView")
